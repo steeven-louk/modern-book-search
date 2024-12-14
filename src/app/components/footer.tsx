568ea254382bd49c0de27f8c9a5ddf2ff5f0daae
steeven-louk <steevenlouk.sl@gmail.com>
2024-12-14 15:59:25 +0100

    (components) mise en place du footer et de la page layout

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 py-8">
+          <div className="container mx-auto px-4 text-center text-gray-600">
+            © 2024 Modern Book Search. Tous droits réservés.
+          </div>
+    </footer>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "./components/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: 'Modern Book Search',
@@ -24,10 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="font-roboto text-gray-900 bg-gray-50">
         {children}
+        <Footer/>
       </body>
     </html>
   );
